a3b41245c814e4b83604bfa78ac0274057ae0c35
Krishna9027 <krishnaguptamnnit45@gmail.com>
2024-06-28 18:50:27 +0530

    completed

---
+++ Frontend/context/GlobalReducer.js
@@ -0,0 +1,11 @@
+export default (state, action) => {
+    switch (action.type) {
+      case "SET_CUSTOMERS":
+        return {
+          ...state,
+          customers: action.payload,
+          loading: false,
+        };
+    }
+  };
+  
